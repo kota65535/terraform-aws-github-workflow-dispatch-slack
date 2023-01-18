998b8217ddeb6bac855be1b51b5e84e774937e34
Tomohiko Ozawa <kota65535@gmail.com>
2023-01-19 01:36:18 +0900

    add renovate workflow

--- pre-commit.sh
+++ pre-commit.sh
@@ -2,12 +2,11 @@
 set -eu
 
 # Install pre-commit hook
-if [[ $0 != "pre-commit" ]]; then
-  cp "$0" .git/hooks/pre-commit
+if [[ $0 != ".git/hooks/pre-commit" ]]; then
+  cp -f "$0" .git/hooks/pre-commit
 fi
 
 # Update requirements.txt
 cd lambda
-poetry lock
 poetry export -o requirements.txt
 git add requirements.txt
