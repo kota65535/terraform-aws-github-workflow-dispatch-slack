e7683abc196796619d7a4fb43307f7e534fd5a08
Tomohiko Ozawa <kota65535@gmail.com>
2023-01-26 00:03:22 +0900

    rename defaults

--- variables.tf
+++ variables.tf
@@ -1,19 +1,19 @@
 variable "lambda_name" {
   description = "Lambda name"
   type        = string
-  default     = "github-workflow-dispatcher"
+  default     = "github-workflow-dispatch-slack"
 }
 
 variable "lambda_iam_role_name" {
   description = "Lambda IAM role name"
   type        = string
-  default     = "github-workflow-dispatcher"
+  default     = "github-workflow-dispatch-slack"
 }
 
 variable "api_gateway_name" {
   description = "API gateway name"
   type        = string
-  default     = "github-workflow-dispatcher"
+  default     = "github-workflow-dispatch-slack"
 }
 
 variable "github_token" {
