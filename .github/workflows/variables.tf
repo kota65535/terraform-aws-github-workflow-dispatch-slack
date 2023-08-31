e335db6cc45ca4f2e0ec7f830a679e8dfef69867
Tomohiko Ozawa <kota65535@gmail.com>
2023-08-31 19:58:08 +0900

    fix

--- .github/workflows/variables.tf
+++ .github/workflows/variables.tf
@@ -3,11 +3,6 @@ variable "github_token" {
   type        = string
 }
 
-variable "slack_bot_token" {
-  description = "Slack bot token"
-  type        = string
-}
-
 variable "slack_verification_token" {
   description = "Slack verification token"
   type        = string
