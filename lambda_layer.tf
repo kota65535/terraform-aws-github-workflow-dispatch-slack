9e6865c0005b18a3cf409a10f094a7d6612e27cd
Tomohiko Ozawa <kota65535@gmail.com>
2025-03-19 13:06:50 +0900

    Use lambda layer module (#30)

--- lambda_layer.tf
+++ lambda_layer.tf
@@ -1,17 +1,9 @@
-data "external" "lambda_layer" {
-  program = ["${local.lambda_dir}/scripts/create_lambda_layer.sh", local.lambda_python_version, "${local.lambda_dir}/requirements.txt"]
-}
+module "lambda_layer" {
+  source  = "kota65535/python-lambda-layer/aws"
+  version = "0.2.0"
 
-data "archive_file" "layer_zip" {
-  type             = "zip"
-  output_path      = local.layer_zip_path
-  source_dir       = data.external.lambda_layer.result.path
-  output_file_mode = "0644"
-}
-
-resource "aws_lambda_layer_version" "version" {
-  layer_name          = var.lambda_name
-  filename            = data.archive_file.layer_zip.output_path
-  compatible_runtimes = ["python${local.lambda_python_version}"]
-  source_code_hash    = data.archive_file.layer_zip.output_base64sha256
+  name              = var.lambda_name
+  python_version    = local.lambda_python_version
+  requirements_path = "${local.lambda_dir}/requirements.txt"
+  output_path       = local.layer_zip_path
 }
